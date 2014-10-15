acae915d9c186526b292ca1e7e488683e5d7bff2
Katie <kab761@gmail.com>
2014-10-15 11:25:04 -0700

    added a bunch of stuff by accident

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,3 +1,19 @@
 class MerchantsController < ApplicationController
 
+  def index
+    @merchants = Merchant.all
+  end
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+    @merchant = Merchant.new(merchant_params)
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:email, :user_name, :shop_name, :description, :password, :password_confirmation)
+  end
 end
