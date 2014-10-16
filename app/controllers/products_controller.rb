99c43dacc4839c0784b42ff3ca4f1bbac1f101fd
Rachelle Keblitis <rkeblitis@gmail.com>
2014-10-15 19:02:58 -0700

    add products view and add create new product to databse method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,16 @@
 class ProductsController < ApplicationController
-  
+
+  def new
+    @product = Product.new
+  end
+
+
+
+
+  def add_to_database
+    @product = Product.new(params.require(:product).permit(:name, :description, :price))
+    @product.save
+    redirect_to "/products/new"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get "/",                to: "home#index"
-  get "/merchants/",      to: "merchants#index"
+  post "products/new",     to: "products#add_to_database"
+  get  "/products/new",    to: "products#new"
+  get  "/",                to: "home#index"
+  get  "/merchants/",      to: "merchants#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
