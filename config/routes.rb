543576e36a26f7fe84fdb3cdf37a56b3972bc631
Rachel <rmoshier@gmail.com>
2014-10-16 11:33:01 -0700

    added products view folder, file and code

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get "/merchants/new",       to: "merchants#new"
   post "/merchants/create",   to: "merchants#create"
   get "/merchants/:id",       to: "merchants#show"
+
+  # Products - Create, view all, edit, and delete
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
