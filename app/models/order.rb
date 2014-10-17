87d8b27301154179b1d8b3c8c7258ec50e34768e
stephanie pi <stephmpi@gmail.com>
2014-10-16 17:51:00 -0700

    fix change quantity functionality

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ActiveRecord::Base
   has_many :merchants
   has_many :order_items
   has_many :products, through: :order_items
+
+  def sorted_order_items
+    order_items.order :product_id
+
+  end
+
 end
