1a26fc62a873120995d6a637cf3c9ebd7772a825
Katie <kab761@gmail.com>
2014-10-17 14:14:06 -0700

    added categories field to new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,10 @@ class ProductsController < ApplicationController
   end
 
   def add_to_database
+    puts params.inspect
     @product = Product.new(params.require(:product).permit(:name, :description, :price))
+    category_names = params.require(:product).permit(:categories)[:categories]
+    add_to_db(category_names)
     @product.save
     redirect_to "/products/index"
   end
@@ -51,5 +54,24 @@ class ProductsController < ApplicationController
   def find_product
     @product = Product.find(params[:id])
   end
+
+  def add_to_db(category_names)
+    if category_names.include?(",")
+      category_names = category_names.split(",")
+    else
+      category_names = category_names.split
+    end
+
+    category_names.each do |category_name|
+      category_name = category_name.strip.downcase
+      cat = Category.find_by(name: category_name)
+      if cat
+        @product.categories << cat
+      else
+        @product.categories << Category.create(name: category_name)
+      end
+    end
+  end
+
 
 end
