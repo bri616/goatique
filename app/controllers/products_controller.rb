1cb0c9eb571af8c8735a32802ce5c5477ef8e97f
Katie <kab761@gmail.com>
2014-10-17 15:29:48 -0700

    added method to add products with categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def add_to_database
-    puts params.inspect
-    @product = Product.new(params.require(:product).permit(:name, :description, :price))
+    @product = Product.new(params.require(:product).permit(:name, :description, :price, :merchant_id))
     category_names = params.require(:product).permit(:categories)[:categories]
     add_to_db(category_names)
     @product.save
@@ -51,27 +50,27 @@ class ProductsController < ApplicationController
   # end
 
   private
-  def find_product
-    @product = Product.find(params[:id])
-  end
-
-  def add_to_db(category_names)
-    if category_names.include?(",")
-      category_names = category_names.split(",")
-    else
-      category_names = category_names.split
+    def find_product
+      @product = Product.find(params[:id])
     end
 
-    category_names.each do |category_name|
-      category_name = category_name.strip.downcase
-      cat = Category.find_by(name: category_name)
-      if cat
-        @product.categories << cat
+    def add_to_db(category_names)
+      if category_names.include?(",")
+        category_names = category_names.split(",")
       else
-        @product.categories << Category.create(name: category_name)
+        category_names = category_names.split
+      end
+
+      category_names.each do |category_name|
+        category_name = category_name.strip.downcase
+        cat = Category.find_by(name: category_name)
+        if cat
+          @product.categories << cat
+        else
+          @product.categories << Category.create(name: category_name)
+        end
       end
     end
-  end
 
 
 end
