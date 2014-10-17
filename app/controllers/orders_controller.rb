c902577a0a051a0d115a96b9fc76e768c0a04d7a
stephanie pi <stephmpi@gmail.com>
2014-10-17 15:03:01 -0700

    add empty method with note in the orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,10 @@ class OrdersController < ApplicationController
     @order = Order.first
   end
 
+  def update_all
+    #method that updates all of the order quantities
+  end
+
   def order_params
     params.require(:mango).permit(:order_item) #can i access the attributes of the other models that Orders is associated with, such as price, etc?
   end
