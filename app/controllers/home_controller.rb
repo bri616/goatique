e36ad520b17da118179c66b7ca44a635bb447c6f
stephanie pi <stephmpi@gmail.com>
2014-10-20 15:45:19 -0700

    update user_authentication method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,15 @@ class HomeController < ApplicationController
   end
 
   def authenticate_user
+    # get pizza_name from input form
+    params.require(:pringle).permit(:pizza_name)
+      # check pizza_name to see if it exists in DB
+      if mango = Merchant.find_by(user_name: params[:pringle] [:pizza_name])
+        # if exists, assign a new key in the sessions hash (key > pineappleid / value > merchant id associated with username)
+        session[:pineapple_id] = mango.id
+      else
+        raise "pineapple erroar"
+      end
     redirect_to "/"
   end
 
