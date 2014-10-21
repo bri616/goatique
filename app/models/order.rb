d14ec25f4f6b05c19f8ba8249947ff243c25425d
Bri Dotson <bridotson@gmail.com>
2014-10-21 15:55:35 -0700

    add method to order model that calculates total number of products

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,15 @@ class Order < ActiveRecord::Base
   has_many :merchants
   has_many :order_items
   has_many :products, through: :order_items
+  attr_accessor :total_number_of_products
+  before_filter :total_number_of_products
 
   def sorted_order_items
     order_items.order :product_id
+  end
 
+  def total_number_of_products
+    order_items.collect(&:product_quantity).reduce :+
   end
 
 end
