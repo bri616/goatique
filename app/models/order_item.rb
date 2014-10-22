20018f532cd0a566d7915996de389bab4cc482b2
Bri Dotson <bridotson@gmail.com>
2014-10-22 13:50:01 -0700

    weird stash problem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,8 +2,12 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
+  before_create :check_against_inventory
 
-
-
+  def check_against_inventory
+    if product_quantity > product.quantity
+      product_quantity = product.quantity
+    end
+  end
 
 end
