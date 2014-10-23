0a754dfa29ada64d73c0b396e729dae6f416bd78
Katie <kab761@gmail.com>
2014-10-23 15:41:57 -0700

    improvements to product display and model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,9 @@
 class Product < ActiveRecord::Base
   validates :merchant_id, presence: true
-  validates :merchant_id, numericality: true
+  validates :merchant_id, numericality: {only_integer: true}
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
-  validates :price, numericality: true
   validates :price, numericality: { greater_than: 0 }
   belongs_to :merchant
   has_many :order_items
