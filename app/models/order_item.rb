02cae0894515e7e0be4953e453560e8eb9c7a8c8
Katie <kab761@gmail.com>
2014-10-23 09:48:33 -0700

    commented out some currently broken validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,15 +3,11 @@ class OrderItem < ActiveRecord::Base
   validates :order_id, numericality: {only_integer: true}
   validates :product_id, presence: true
   validates :product_id, numericality: {only_integer: true}
-  validates :quantity, presence: true
-  validates :quantity, numericality: {only_integer: true}
-  validates :quantity, numericality: {greater_than: 0}
+  validates :product_quantity, presence: true
+  validates :product_quantity, numericality: {only_integer: true}
+  validates :product_quantity, numericality: {greater_than: 0}
 
   belongs_to :order
   belongs_to :product
-
-
-
-
 
 end
