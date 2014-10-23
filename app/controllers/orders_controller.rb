f25db668bc103dda9f58ea305f3491a6dab6c112
stephanie pi <stephmpi@gmail.com>
2014-10-23 13:42:44 -0700

    update stock quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,9 +19,16 @@ class OrdersController < ApplicationController
      @current_order.update(params.require(:order).permit(:first_name, :last_name, :mailing_address, :email, :card_number, :expiration_date))
 
      #change status to paid
+
      if @current_order.update(order_status: "Paid")
-       raise "Yay paid!"
        #   iterate through order_items and update_stock based on #of items ordered(needs to be in model bc relates to DB)
+       puts @current_order.order_items.first.product.quantity
+       @current_order.order_items.each do |order_item|
+         new_stock = order_item.product.quantity - order_item.product_quantity
+         order_item.product.update(quantity: new_stock)
+       end
+       puts @current_order.order_items.first.product.quantity
+
        #   set current  session order id to nil
        #   redirect_to new confirmation page (maybe refactor to partial)
      else
