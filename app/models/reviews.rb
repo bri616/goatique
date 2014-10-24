728d19e61976604079634d5a22c86cbb3b76d910
Rachelle Keblitis <rkeblitis@gmail.com>
2014-10-23 17:14:03 -0700

    unsure what is going on

--- app/models/reviews.rb
+++
@@ -1,6 +0,0 @@
-class Reviews < ActiveRecord::Base
-  validates :rating, presence: true
-  validates :rating, numericality: {only_integer: true}
-  validates :rating, inclusion: {in: 1..5}
-  belongs_to :product
-end
